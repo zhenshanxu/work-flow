f38d77703dbd47261ccdedf9bb21dc3d1d0f8834
xuzhenshan <xuzhenshanvip@163.com>
2021-02-01 11:31:43 +0800

    first commit

---
+++ src/main/java/com/example/workflow/WorkFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workflow/WorkFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
