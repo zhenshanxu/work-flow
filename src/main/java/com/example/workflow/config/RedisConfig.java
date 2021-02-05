a97b08384a1179c6a0796966aef9c1a307479cb1
xuzhenshan <xuzhenshan@163.com>
2021-02-05 15:51:08 +0800

    feat:配置Redis

---
+++ src/main/java/com/example/workflow/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.example.workflow.config;
+
+/**
+ * @BelongsProject: work-flow
+ * @BelongsPackage: com.example.workflow.config
+ * @Author: ZhenShan Xu
+ * @CreateTime: 2021-02-05 14:40.
+ * @Description:
+ */
+
+public class RedisConfig {
+}
