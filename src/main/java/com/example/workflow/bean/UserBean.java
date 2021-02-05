44be9618e38b5e16b051060ba33ee8ef90bf618b
xuzhenshan <xuzhenshan@163.com>
2021-02-05 09:49:35 +0800

    feat : 添加lombok配置文件

---
+++ src/main/java/com/example/workflow/bean/UserBean.java
@@ -0,0 +1,22 @@
+package com.example.workflow.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @BelongsProject: work-flow
+ * @BelongsPackage: com.example.workflow.Bean
+ * @Author: ZhenShan Xu
+ * @CreateTime: 2021-02-05 09:45.
+ * @Description: 用户信息
+ */
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserBean {
+
+}
