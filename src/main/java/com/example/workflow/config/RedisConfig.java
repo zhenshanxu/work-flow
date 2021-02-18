4cfa377fac30e901de261c84ff6d4b9c9a4937b8
xuzhenshan <xuzhenshan@163.com>
2021-02-18 09:49:36 +0800

    feat :add redis config file

--- src/main/java/com/example/workflow/config/RedisConfig.java
+++ src/main/java/com/example/workflow/config/RedisConfig.java
@@ -1,5 +1,12 @@
 package com.example.workflow.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 /**
  * @BelongsProject: work-flow
  * @BelongsPackage: com.example.workflow.config
@@ -7,6 +14,26 @@ package com.example.workflow.config;
  * @CreateTime: 2021-02-05 14:40.
  * @Description:
  */
-
+@Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        //使用StringRedisSerializer来序列化和反序列化redis的ke
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        //开启事务
+        redisTemplate.setEnableTransactionSupport(true);
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
 }
